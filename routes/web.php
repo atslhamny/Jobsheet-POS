4433123a99ea3db9138cd4c947c25359e48633c3
Atsilah Amany <atsilahamanyph@gmail.com>
2025-03-13 23:11:19 +0700

    JBS 3:Praktikum 5 – Implementasi Query Builder

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
@@ -11,6 +12,7 @@ use App\Http\Controllers\SalesController;
 Route::get('/', [HomeController::class, 'index']);
 
 Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
 
 
 // Halaman Products dengan Prefix Route
