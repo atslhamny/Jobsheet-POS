3e5b6ba82c760f40aac2f5af5cc673434a52fb7a
Atsilah Amany <atsilahamanyph@gmail.com>
2025-03-16 19:49:24 +0700

    JBS 4: Praktikum 2.3 – Retreiving Aggregrates

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -26,7 +26,9 @@ class UserController extends Controller
 
         // $user = UserModel::all();
         // $user = UserModel::findOrfail(1);
-        $user = UserModel::where('username', 'manager9')->firstOrFail();
+        // $user = UserModel::where('username', 'manager9')->firstOrFail();
+        $user = UserModel::where('level_id', 2)->count();
+        // dd($user);
         return view('user', ['data' => $user]);
     }
 }
