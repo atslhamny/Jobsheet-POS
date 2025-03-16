4971ae4d9f336b9f39577bed958133b9641228c9
Atsilah Amany <atsilahamanyph@gmail.com>
2025-03-16 21:57:56 +0700

    JBS 4: Praktikum 2.6 – Create, Read, Update, Delete (CRUD)

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -7,6 +7,7 @@
 
     <body>
         <h1>Data User</h1>
+        <a href="/Jobsheet-POS/public/user/tambah">+ Tambah User</a>
         <table border="1" cellpadding="2" cellspacing="0">
             <tr>
                 <th>ID</th>
@@ -18,14 +19,15 @@
             {{-- <tr>
                 <td>{{ $data }}</td>
             </tr> --}}
-            {{-- @foreach ($data as $d) --}}
+            @foreach ($data as $d)
             <tr>
-                <td>{{ $data->user_id }}</td>
-                <td>{{ $data->username }}</td>
-                <td>{{ $data->nama }}</td>
-                <td>{{ $data->level_id }}</td>
+                <td>{{ $d->user_id }}</td>
+                <td>{{ $d->username }}</td>
+                <td>{{ $d->nama }}</td>
+                <td>{{ $d->level_id }}</td>
+                <td><a href="/Jobsheet-POS/public/user/ubah/{{ $d->user_id }}">UBAH</a> | <a href="/Jobsheet-POS/public/user/hapus/{{ $d->user_id }}">HAPUS</a></td>
             </tr>
-            {{-- @endforeach --}}
+            @endforeach
         </table>
     </body>
 
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,17 @@ Route::get('/', [HomeController::class, 'index']);
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
 Route::get('/user', [UserController::class, 'index']);
+Route::get('/user/tambah', [UserController::class, 'tambah']);
+Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
+Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
+Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
+Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+
+
+
+
+
+
 
 // Halaman Products dengan Prefix Route
 Route::prefix('category')->group(function () {
